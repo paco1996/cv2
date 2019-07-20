574603b826aa7aff7874ccb868282768833a5fea
paco1996 <paco_cg96@hotmail.com>
2019-07-20 15:38:12 -0500

    Add files via upload

---
+++ correo.php
@@ -0,0 +1,11 @@
+<?php
+    ini_set( 'display_errors', 1 );
+    error_reporting( E_ALL );
+    $from = "paco_cg96@hotmail.com";
+    $to = "paco240296@gmail.com";
+    $subject = "Checking PHP mail";
+    $message = "PHP mail works just fine";
+    $headers = "From:" . $from;
+    mail($to,$subject,$message, $headers);
+    echo "The email message was sent.";
+);
