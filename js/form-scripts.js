58aef002ca02e732442dc2d4de53545fd1c74246
paco1996 <paco_cg96@hotmail.com>
2019-07-24 12:01:02 -0500

    Add files via upload

---
+++ js/form-scripts.js
@@ -0,0 +1,69 @@
+$("#contactForm").validator().on("submit", function (event) {
+    if (event.isDefaultPrevented()) {
+        // handle the invalid form...
+        formError();
+        submitMSG(false, "¿Has rellenado el formulario correctamente?");
+    } else {
+        // everything looks good!
+        event.preventDefault();
+        submitForm();
+    }
+});
+
+(function(){
+                 emailjs.init("user_v8KTzHwITHzM4e1b0UJIv");
+             })();
+
+function submitForm(){
+    //$( "#form-submit" ).prop( "disabled", true );
+    // Initiate Variables With Form Content
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+    
+
+           
+                        var data = {
+                           from_name: this.from_name=name,
+                            from_email: this.from_email=email,
+                            message_html: this.message_html=message,
+                        };
+                        
+                        emailjs.send("gmail", "template_XxEillND_clone", data)
+                        
+                        .then(function(response) {
+                            if(response.text === 'OK'){
+                                formSuccess();
+                            }
+                           console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+                        }, function(err) {
+                           formError();
+                           submitMSG(false,"FAILED. error="+err);
+                        });
+                    
+}
+
+
+
+
+function formSuccess(){
+    $("#contactForm")[0].reset();
+    submitMSG(true, "Mensaje enviado!")
+}
+
+function formError(){
+    $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+        $(this).removeClass();
+    });
+}
+
+function submitMSG(valid, msg){
+    if(valid){
+        var msgClasses = "h3 text-center tada animated text-success";
+    } else {
+        var msgClasses = "h3 text-center text-danger";
+    }
+    $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+}
+
+
